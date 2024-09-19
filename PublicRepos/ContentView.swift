897ad9a93080cb4278abafc70116aa6360bce02e
Mariam <mariam.khaled.elenna@gmail.com>
2024-09-19 18:35:36 +0300

    Initial Commit

---
+++ PublicRepos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PublicRepos
+//
+//  Created by Mariam on 19/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PublicRepos/PublicReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicReposApp.swift
+//  PublicRepos
+//
+//  Created by Mariam on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PublicReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
